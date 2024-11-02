98a4228a0722c166f01a22287adb4065f25b3618
Vasanth <vasanthprabhakaran1999@gmail.com>
2024-11-02 19:44:23 +0530

    .

--- src/app/components/Product/SwiperSlider.tsx
+++ src/app/components/Product/SwiperSlider.tsx
@@ -1,16 +1,13 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import SwiperSliderContainer from './SwiperSliderContainer';
-import { productsByCategory } from '@/app/utils/utils';
+import { productsByCategory } from '../../utils/utils';
 
 export default function SwiperSlider({ collection }: any) {
   const [products, setProducts] = useState<any>([]);
-  const fetchProducts = () => {
+  useEffect(() => {
     const data = productsByCategory[`${collection}`];
     setProducts(data);
-  };
-  useEffect(() => {
-    fetchProducts();
   }, [collection]);
   return (
     <div className="max-w-[1200px]">
